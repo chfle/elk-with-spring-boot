3b0467a02782c4039bf614102530c532ad7a3c85
chfle <christian.lehnert.home@gmail.com>
2023-05-13 22:05:34 +0200

    code base

---
+++ src/main/java/org/lehnert/ELK_example/ElkExampleApplication.java
@@ -0,0 +1,13 @@
+package org.lehnert.ELK_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkExampleApplication.class, args);
+	}
+
+}
